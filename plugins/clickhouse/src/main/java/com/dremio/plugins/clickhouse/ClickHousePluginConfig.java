3571e322a14ca39b5374197513d7dfa1359dc982
Auto Reflection System <2773020689@qq.com>
2026-03-26 11:33:50 +0800

    fix(clickhouse): Fix port configuration and simplify dependencies
    - Changed default port from 18124 to 18123 (HTTP interface)
    - Simplified pom.xml by removing unnecessary Arrow Flight dependencies
    - Using only ClickHouse JDBC driver (v0.6.3) for connectivity

--- plugins/clickhouse/src/main/java/com/dremio/plugins/clickhouse/ClickHousePluginConfig.java
+++ plugins/clickhouse/src/main/java/com/dremio/plugins/clickhouse/ClickHousePluginConfig.java
@@ -37,7 +37,7 @@ public class ClickHousePluginConfig extends ConnectionConf<ClickHousePluginConfi
 
   @NotNull
   @Tag(2)
-  public int port = 18124;  // ClickHouse native port (mapped from 9000)
+  public int port = 18123;  // ClickHouse HTTP interface (mapped from 8123)
 
   @Tag(3)
   @Username
